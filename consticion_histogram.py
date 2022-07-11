ad0a36e47fea56a90fa200531e5a063289757dc2
noceive <nocivescience@gmail.com>
2022-07-11 18:49:30 -0400

    finalizado

--- consticion_histogram.py
+++ consticion_histogram.py
@@ -1,5 +1,26 @@
+from cv2 import rectangle
 from manim import *
 class ConstHistScene(Scene):
     def construct(self):
-        number_list=[1,2,3,4,5,6,7,8,9,10]
-        
+        number_list=[np.random.random()*10 for i in range(100)]
+        number_tracker=ValueTracker(1)
+        rectangle=self.get_rectangle()
+        rectangle.add_updater(
+            lambda t: self.set_rectangle(rectangle,number_tracker.get_value())
+        )
+        self.add(rectangle)
+        for value in number_list:
+            anims=[
+                ApplyMethod(
+                    number_tracker.set_value,value,
+                    rate_func=linear,
+                    run_time=.1,
+                )
+            ]
+            self.play(*anims)
+        self.wait()
+    def get_rectangle(self):
+        rectangle=Rectangle()
+        return rectangle
+    def set_rectangle(self,rectangle,scale):
+        rectangle.stretch_to_fit_height(scale)
